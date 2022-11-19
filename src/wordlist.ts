baa6b68dba93e0c3a1c68d75800efa0debae24a9
David Klötzer <david.kloetzer@casavi.de>
2022-11-19 13:33:12 +0100

    Optimize for 2x5 words

--- src/wordlist.ts
+++ src/wordlist.ts
@@ -1,6 +1,6 @@
-export const words_raw = `si so so sa mo ma am im
+export const words_raw = `si so so sa om mo ma is am im
 sisi sasa sao siso sosi Sisi Mama Mimi Oma Sisi
-um us su sa mu mi ui im
+um us su sa us mu mi ui im so
 Susi Mua Ami Maus Susa so Susi Maus Susu Imu`;
 
 export function convert_wordlist(words_raw: string): string[][] {
