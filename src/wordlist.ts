4e438a18b354afb174be9c023eeb739a026267b8
David Klötzer <david.kloetzer@casavi.de>
2022-11-22 22:08:49 +0100

    feat: add words with R

--- src/wordlist.ts
+++ src/wordlist.ts
@@ -1,10 +1,14 @@
 export const words_raw = `si so so sa om mo ma is am im
 sisi sasa sao siso sosi Sisi Mama Mimi Oma Sisi
 um us su sa us mu mi ui im so
-Susi Mua Ami Maus Susa so Susi Maus Susu Imu`;
+Susi Mua Ami Maus Susa so Susi Maus Susu Imu
+ir ri ra ro ar or ur ru
+riri rara roro ruru ruri rari romu rimu roma
+rosa rosi roso rosu Rom Moor mir sar Mimi Ami
+Rosi rosa Rom risa Siri Irmi arm am im Arm`;
 
 export function convert_wordlist(words_raw: string): string[][] {
-    return words_raw.split('\n').map(line => line.split(' '));
+  return words_raw.split("\n").map((line) => line.split(" "));
 }
 
 export const wordlist = convert_wordlist(words_raw);
